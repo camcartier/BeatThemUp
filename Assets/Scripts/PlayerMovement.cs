c4cea7acb6ce60decf604ec0d1b52cd1f83685b7
Iyachtu <122690458+Iyachtu@users.noreply.github.com>
2023-02-03 10:38:39 +0100

    added basic movement and player input

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,9 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private Rigidbody2D _rb;
+    [SerializeField] private InputActionReference movement, attack, jump;
+    private Vector2 _move;
+    private float _movespeed = 5;
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +24,31 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        GetInput();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void GetInput()
+    {
+        _move = movement.action.ReadValue<Vector2>();
+    }
+
+    private void Move()
+    {
+        _rb.velocity = _move * _movespeed * Time.fixedDeltaTime ;
+    }
+
+    private void Jump()
+    {
+
+    }
+
+    private void Attack()
+    {
+
     }
 }
