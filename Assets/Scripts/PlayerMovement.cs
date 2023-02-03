48134d7df55e6fc50328a442728f061e8f7096b2
FishyTree <53624883+camcartier@users.noreply.github.com>
2023-02-03 15:45:29 +0100

    Changes to PlayerScript

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,12 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+enum PlayerStateMode
+{
+    IDLE,
+    WALK,
+    JUMP,
+    ATTACK
+}
+
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D _rb;
     [SerializeField] private InputActionReference movement, attack, jump;
     private Vector2 _move;
-    private float _movespeed = 5;
+    private float _movespeed = 60;
+    private bool _isWalking;
+
+
+    //valeur recuperee (0>>1) lorsque le bouton est active
+    private float _isJumping;
+    [SerializeField] private float _jumpForce = 2f;
+
+    //valeur recuperee (0>>1) lorsque le bouton est active
+    private float _isAttacking;
+
+    //gestion de l'animator
+    private PlayerStateMode _currentState;
+    private Animator _animator;
+
 
     private void Awake()
     {
@@ -30,25 +52,110 @@ public class PlayerMovement : MonoBehaviour
     private void FixedUpdate()
     {
         Move();
+        if (_isJumping != 0)
+        {
+            Jump();
+        }
+
     }
 
     private void GetInput()
     {
         _move = movement.action.ReadValue<Vector2>();
+        _isJumping = jump.action.ReadValue<float>();
+        _isAttacking= attack.action.ReadValue<float>(); 
     }
 
     private void Move()
     {
         _rb.velocity = _move * _movespeed * Time.fixedDeltaTime ;
+        
     }
 
     private void Jump()
     {
-
+        _rb.AddForce(transform.up *_jumpForce, ForceMode2D.Impulse);
     }
 
     private void Attack()
     {
+        
+    }
 
+    #region Testing Debug
+    /*
+    void OnJump()
+    {
+        Debug.Log("Jump pressed");
+
+    }
+
+    void OnAttack()
+    {
+        Debug.Log("Attack");
+
+    }*/
+    #endregion
+
+    void OnStateEnter()
+    {
+        switch (_currentState)
+        {
+            case PlayerStateMode.IDLE:
+                break;
+
+            case PlayerStateMode.WALK:
+
+                _animator.SetBool("Walking", true);
+                break;
+
+            case PlayerStateMode.JUMP:
+
+                _animator.SetBool("Jumping", true);
+                break;
+
+            case PlayerStateMode.ATTACK:
+
+                _animator.SetBool("Attacking", true);
+                break;
+        }
+    }
+
+    void OnStateUpdate()
+    {
+        switch (_currentState)
+        {
+            case PlayerStateMode.IDLE:
+                break;
+
+            case PlayerStateMode.WALK:
+
+                if (_isJumping>0)
+                {
+                    TransitionToState(PlayerStateMode.JUMP);
+                }
+                if (_isAttacking>0)
+                {
+                    TransitionToState(PlayerStateMode.ATTACK);
+                }
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    void OnStateExit()
+    {
+
+    }
+
+    void TransitionToState(PlayerStateMode toState)
+    {
+        OnStateExit();
+
+        _currentState = toState;
+
+        OnStateEnter();
     }
 }
