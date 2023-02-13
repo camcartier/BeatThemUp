40e0cc05a24042196dbf81434aa4eb3d6595c1db
FishTree <53624883+camcartier@users.noreply.github.com>
2023-02-13 11:03:42 +0100

    modifs sprite robotnik et nouveau script control pour regler le pb de casse

--- Assets/Scripts/ScoreControls.cs
+++ Assets/Scripts/ScoreControls.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
-public class ScoreControl : MonoBehaviour
+public class ScoreControls : MonoBehaviour
 {
     private TMP_Text _counterTxt;
     private int _changeTracker;
